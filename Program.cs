663bb6ec808a984c0f108425407b7339e9584e46
Warre Gehre <s131007@ap.be>
2023-01-05 18:27:17 +0100

    Program.cs push/upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Warre_Gehre_GameDevelopment.GameFiles;
+
+namespace Warre_Gehre_GameDevelopment.GameFiles
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
+
+
